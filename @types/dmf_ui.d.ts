16688ee37daa5e12dabad54195a230d06a914949
NGHIA Nguyen Duc Dai <nghia.nguyenduc@eightcap.com>
2022-08-08 16:48:36 +0700

    Updates

--- @types/dmf_ui.d.ts
+++ @types/dmf_ui.d.ts
@@ -144,6 +144,7 @@ declare module "dmf_ui/label/ClosingLabel" {
 }
 declare module "dmf_ui/Button" {
     import React from 'react';
+    import { Color } from "dmf_ui/components/core/Core";
     type Props = {
         buttonType?: 'fill' | 'outline' | 'link';
         block?: boolean;
@@ -151,9 +152,10 @@ declare module "dmf_ui/Button" {
         className?: string;
         ripple?: 'dark' | 'light';
         size?: 'md' | 'sm' | 'lg';
-        color?: 'primary' | 'secondary';
+        color?: Color;
         iconOnly?: boolean;
         children?: React.ReactNode;
+        uppercase?: boolean;
         onClick?: (event: Event) => void;
     };
     const Button: React.ForwardRefExoticComponent<Props & React.RefAttributes<any>>;
@@ -895,5 +897,432 @@ declare module "dmf_ui/dynamicForm" {
         renderInputs(inputs: FormInput[], isRow?: boolean): JSX.Element;
         renderInput(inputs: FormInput | FormInput[]): JSX.Element;
         getValidatoinError(input: FormInput): string;
+    }
+}
+declare module "dmf_ui/components/Table/table-types" {
+    import { Color } from "dmf_ui/components/core/Core";
+    export interface Icolumn {
+        field: string;
+        use?: string;
+        useInSearch?: boolean;
+        useInDisplay?: boolean;
+        useInExport?: boolean;
+    }
+    export type Irow = Record<string, any>;
+    /**
+     * A function whose return value is either a JSX element or a string
+     */
+    export type renderFunction = (row: Irow, col: Icolumn, display_value: string) => JSX.Element | string;
+    /**
+     * A function when invoked, returns a string.
+     */
+    export type stringRenderFunc = (row: Irow, col: Icolumn, display_value: string) => string;
+    export interface Iprop {
+        rows: Irow[];
+        columns: Icolumn[];
+        perPage?: number;
+        noContentText?: string;
+        debounceSearchTime?: number;
+        tableHeader?: string;
+        showSearch?: boolean;
+        shouldExport?: boolean;
+        exportText?: string;
+        bulkSelectOptions?: string[];
+        bulkSelectButtonText?: string;
+        exportCsvFile?: string;
+        striped?: boolean;
+        bordered?: boolean;
+        hovered?: boolean;
+        rowRender?: renderFunction;
+        onSearch?: (search_word: string, result?: Irow[] | []) => void;
+        /**
+         * A function used in modifying a row member before it is used for downloading an excel sheet
+         */
+        exportModify?: stringRenderFunc;
+        onBulkAction?: (selected_option: string, selected: Irow[]) => void;
+        styling?: ItableStyle;
+    }
+    export interface ItableStyle {
+        iconFamily?: 'material' | 'awesome';
+        baseBgColor?: Color;
+        baseTextColor?: Color;
+        main?: string;
+        top?: ItableTop;
+        table_head?: {
+            table_row?: string;
+            table_data?: string;
+        };
+        table_body?: {
+            main?: string;
+            table_row?: string;
+            table_data?: string;
+        };
+        footer?: {
+            main?: string;
+            statistics?: {
+                main?: string;
+                bold_numbers?: string;
+            };
+            page_numbers?: string;
+        };
+    }
+    export interface ItableTop {
+        title?: string;
+        elements?: {
+            main?: string;
+            search?: {
+                text?: string;
+                placeHolder?: string;
+                icon?: string;
+            };
+            bulkSelect?: {
+                main?: string;
+                button?: string;
+            };
+            export?: string;
+        };
+    }
+    export interface Istate {
+        /**
+         * The rows currently being shown on the table or rows for the current active page number
+         */
+        active_rows: Irow[] | [];
+        /**
+         * All page numbers are stored in an array
+         */
+        page_number_list: number[];
+        /**
+         * Holds the current active page number
+         */
+        active_page_number: number;
+        /**
+         * An `OBJECT MAP` whose key is each page number and points to
+         * the
+         */
+        paginated_map: IPaginated;
+        /**
+         * A number that holds the count of all rows
+         */
+        total_rows_count: number;
+    }
+    export interface ItableLinks {
+        /**
+         * An array to store the page numbers
+         */
+        page_number_list: number[];
+        /**
+         * An `OBJECT MAP` whose key is each page number and points to
+         * the `IperPage` data for the page.
+         */
+        page_map: IPaginated;
+        /**
+         * a count of the pages just iterated
+         */
+        total_data_count: number;
+    }
+    /**
+     * `Record like` Interface describing how the object holding each page data will look like
+     */
+    export interface IPaginated {
+        [key: number]: IPerPage;
+    }
+    export interface IretainPage {
+        active_number: number;
+        page_map?: IPaginated;
+    }
+    export interface IPerPage {
+        /**
+         * All row data for this page
+         */
+        page_row_array: Irow[];
+        /**
+         * If this page is active, can the pagination back button be clickable ?
+         */
+        back_button_clickable: boolean;
+        /**
+         * If this page is active, can the Forward pagination button be clickable ?
+         */
+        forward_button_clickable: boolean;
+        is_active: boolean;
+        /**
+         * The array index this records starts from
+         */
+        from_index: number;
+        /**
+         * A set that holds the index of all rows that a marked as checked(selected) for bulk
+         * actions on this page. This holds data each page(paginated rows). This means if there are 20 pages, each page will have
+         * its own `Set`, this 20 Sets in `ItableLinks.page_map` data
+         */
+        checked_set: Set<number>;
+    }
+}
+declare module "dmf_ui/components/Table/utilsClass" {
+    import { Irow, Icolumn, ItableLinks, IretainPage, stringRenderFunc } from "dmf_ui/components/Table/table-types";
+    /**
+     * This class helps run minor utilities needed by our application to quickly process
+     * data and get stuff done
+     */
+    export default class utilsClass {
+        static regex_special: string[];
+        /**
+         * Performs a left trim as done by
+         * {@link utilsClass.trim}
+         * Thus removing a single or repeating character from the rightmost part of
+         * a string
+         */
+        static ltrim(trimString: string, regexp: string, replaceWith: string, more?: boolean, allowRegex?: boolean): string;
+        static isInteger(input: any): boolean;
+        /**
+         * Performs a right trim as done by
+         * {@link utilsClass.trim}
+         * Thus removing a single or repeating character from the leftmost part of
+         * a string
+         */
+        static rtrim(trimString: string, regexp: string, replaceWith: string, more?: boolean, allowRegex?: boolean): string;
+        /**
+         * Trims a sertain character from a string. This does both left and right trimming
+         *
+         * @param trimString  The string to be trimmed.
+         * @param regexp A character to remove from the string.
+         * @param replaceWith  The character or string to put in place of the replaced string.
+         * @param more  if `true`, then it replacement occures for more than a single occurence, else it replaces just
+         * One occurence.
+         ** @param dir  Decides if it should be a right or left trim
+         ** @param allowRegex  Does not escape input search element
+         *
+         */
+        static trim(trimString: string, regexp: string, replaceWith: string, more: boolean, dir: "r" | "l", allowRegex?: boolean): string;
+        /**
+         * A Function to determine if two objects are arrays are equal
+         *
+         * V = value, O= object, O = other,T = Type, OOT =  Other Object Type
+         */
+        static isObjectEqual<VT, OT, VOT extends Record<string, any>, OOT extends Record<string, any>>(value: VT[] | VOT, other: OT[] | OOT): boolean;
+        /**
+         * Compare Primitives, Might be number, string, array , objects or  arrays
+         */
+        static compare<Item1T, Item2T, obj1T extends Record<string, any>, Obj2T extends Record<string, any>>(item1: string | number | Item1T[] | obj1T, item2: string | number | Item2T[] | Obj2T): boolean;
+        /**
+         * Tests if this given value is even.
+         * @returns {boolean}
+         */
+        static isEven(value: number): boolean;
+        /**
+         * Internal function used to implement `utilsClass.throttle` and `utilsClass.debounce`.
+         * @param {Function} func: function to be invoked.
+         * @param {number} wait: The amount of milliseconds to wait before executing the said function.
+         *
+         * Borrowed from https://github.com/jashkenas/underscore/commit/9e3e067f5025dbe5e93ed784f93b233882ca0ffe
+         */
+        static limit<D extends Function>(func: D, wait: number, debounce: boolean): (...input: any) => void;
+        /**
+         *  Returns a function, that, when invoked, will only be triggered at most once
+         *  during a given window of time.
+         *
+         *  TODO: allow to set if this callback should be invoked at the
+         *            "Beginning" or "End" of the time out
+         *            Currently it is invoked at the end
+         */
+        static throttle: (func: Function, wait: number) => (...input: any) => void;
+        /**
+         * Returns a "function"(rememeber to implement the function), that,
+         * as long as it continues to be invoked, will not
+         * be triggered. The function will be called after it stops being called for
+         * N milliseconds.
+         */
+        static debounce: (func: Function, wait: number) => (...input: any) => void;
+        static unwindObject(obj: Record<string, any>, field: string): string;
+        static generateCSV(columns: Icolumn[], items: Irow[], exportModify?: stringRenderFunc): Promise<Blob>;
+        /**
+          * Recursively stringifies the values of an object, space separated, in an
+          * SSR safe deterministic way (keys are storted before stringification)
+          *
+          *   ex:
+          *    ```json
+          *     { b: 3,
+          *       c: { z: 'zzz', d: null, e: 2 },
+          *       d: [10, 12, 11],
+          *       a: 'one'
+          *     }
+          *    ```
+          *   becomes
+          *     'one 3 2 zzz 10 12 11'
+          *
+          * Primatives (numbers/strings) are returned as-is
+          * Null and undefined values are filtered out
+          * Dates are converted to their native string format
+          */
+        static stringifyObjectValues(val: Record<string, any> | any): string;
+        static sanitizeRow(row: Irow, columns: Icolumn[]): object;
+        static stringifyRowValues(row: Irow, column: Icolumn[]): string;
+        /**
+          *
+          * Generates a filter function for the search string
+          */
+        static filterFunction(search_string: string, columns: Icolumn[]): (row: Irow) => boolean;
+        /**
+           * Build the RegExp (no need for global flag, as we only need
+           * to find the value once in the string)
+           * @param {string} str: The input text
+           * @return {RegExp} The returned regex match
+           */
+        static stringtoRegEx(str: string): RegExp;
+        static RX_SPACES$1: RegExp;
+        static matchMultipleSpace(str: string): string;
+        /**
+         *  Borrowed this function from SENTRY(The remote error loggers)
+         *
+         * Escapes special characters, except for whitespace, in a string to be
+         * used inside a regular expression as a string literal.
+         * @param {string} text The string.
+         * @return {string} The escaped string literal.
+         */
+        static escapeRegExp(text: string): string;
+        /**
+         * This function helps me paginate data and present the paginated data to the table component
+         *
+         * @param {string} rows The rows of data to be paginated.
+         * @param {string} per_page An integer
+         * @param {object} retain_expage_map Recently, I wanted to re-render components while using an updated row data(page_row_array)
+         * but retaining all other property in the page_map. Thus I used this to do keep the
+         * former pagemap I will be getting the data from
+         * @return {ItableLinks} The Final data containing the paginated data and an array of all
+         *  pages involved in the pagination i.e 1,2,3,4,5 ..... n
+         */
+        static TableNumberLinks(rows: Irow[], per_page?: number, retain_expage_map?: IretainPage): ItableLinks;
+    }
+}
+declare module "dmf_ui/components/pagination/Pagination/index" {
+    export { default } from "dmf_ui/pagination/Pagination";
+}
+declare module "dmf_ui/components/pagination/PaginationItem/index" {
+    export { default } from "dmf_ui/pagination/PaginationItem";
+}
+declare module "dmf_ui/components/typography/Heading4/index" {
+    export { default } from "dmf_ui/typography/H4";
+}
+declare module "dmf_ui/components/Table/table-sub-components" {
+    import React from 'react';
+    import { Color } from "dmf_ui/components/core/Core";
+    import { Icolumn, Irow, renderFunction, IPaginated, IPerPage, stringRenderFunc } from "dmf_ui/components/Table/table-types";
+    export interface ItableRow {
+        row: Irow;
+        columns: Icolumn[];
+        /**
+         * If users want to enable using bulk selects, then they have to pass in an array of strings of actions.
+         * If that array of string is empty, then this will be false
+         */
+        use_bulk_action: boolean;
+        index: number;
+        render?: renderFunction;
+        checked_set: Set<number>;
+        active_page_number: number;
+        /**
+         * Whenever a single checkbox in a table row is clicked, this is invoked. Since I'm using a cotrolled input,
+         * I therefore trigger an event up the tree to notify about this click. This is then recorded on the
+         * `Set` that holds data of clicked rows for this page.
+         */
+        setCheck: (page_number: number, index: number, ischecked: boolean) => void;
+        is_striped: boolean;
+        is_bordered: boolean;
+        is_hovered: boolean;
+        row_style: string;
+        data_style: string;
+    }
+    export function TableRow(props: ItableRow): JSX.Element;
+    export function TableData(props: {
+        col: Icolumn;
+        row: Irow;
+        render?: renderFunction;
+        style: string;
+    }): JSX.Element;
+    interface Ithead {
+        columns: Icolumn[];
+        useBulkAction: boolean;
+        pageData: IPerPage;
+        activePageNumber: number;
+        mass_checking: (page_number: number, action: 'check-all' | 'uncheck-all') => void;
+        row_style: string;
+        data_style: string;
+    }
+    export function TableHead(props: Ithead): JSX.Element;
+    export function TableCaption(props: {
+        text: string;
+        className: string;
+    }): JSX.Element;
+    export function TableSearch(props: {
+        onSearch: (text: string) => void;
+        label?: string;
+        placeHolder?: string;
+        iconFamily?: 'material' | 'awesome';
+    }): JSX.Element;
+    interface IexportProps {
+        export_text: string;
+        paginated_data: IPaginated;
+        cols: Icolumn[];
+        file_name: string;
+        processFunc?: stringRenderFunc;
+        color: Color;
+    }
+    export function TableExport(props: IexportProps): JSX.Element;
+    export function TableTop(props: {
+        children?: React.ReactNode;
+        style: string;
+    }): JSX.Element;
+    export interface ItableActProps {
+        action_options: string[];
+        eventSelected: (option: string) => void;
+        bgColor?: Color;
+        text_color?: string;
+        dropdown_style: string;
+        button_style: string;
+        button_text: string;
+    }
+    export function TableBulkAction(props: ItableActProps): JSX.Element;
+    interface Ifooter {
+        pageNumberList: number[];
+        paginatedMap: IPaginated;
+        activePage: number;
+        totalPages: number;
+        onPageChange: (page_number: number) => void;
+    }
+    export function Footer(props: Ifooter): JSX.Element;
+}
+declare module "dmf_ui/DataTable" {
+    import React from 'react';
+    import { Iprop, Istate } from "dmf_ui/components/Table/table-types";
+    export * from "dmf_ui/components/Table/table-types";
+    export default class DataTable extends React.Component<Iprop, Istate> {
+        props: Iprop;
+        static propTypes: {};
+        static defaultProps: {};
+        state: Istate;
+        /**
+         * When ever a search happens, it is debounced for `this.props.debounce_search` seconds in case user
+         * input is steady. This property holds the debounced funtion gotten from @see {@link utilsClass.debounce}
+         */
+        private processFilterdebounce;
+        constructor(props: Iprop);
+        componentDidUpdate(prevProps: Iprop, prevState: Istate, _snapshot: any): void;
+        private bulkActClick;
+        private onSearch;
+        /**
+         * A function that uses a promise to filter all rows using a search keyword typed into the
+         * Search input bar.
+         * @param filter_function
+         */
+        private processFilter;
+        private massChecking;
+        private checkBoxCheck;
+        private getTotalPages;
+        private onPageChange;
+        private initialPaginateSetup;
+        /**
+         * Checks if there are bulk selected items in the table
+         * @returns boolean
+         */
+        private isBulkSelected;
+        private getStylingProps;
+        render(): JSX.Element;
     }
 }
